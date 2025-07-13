98cc618b662bd98b713d0a7149738b0322651112
Jstevemanuel <165416267+Jstevemanuel@users.noreply.github.com>
2025-07-13 23:51:38 +0530

    Add files via upload

---
+++ W3-Exercise1-Configuring a basic SpringApplication/src/main/java/com/library/LibraryManagementApplication.java
@@ -0,0 +1,21 @@
+package com.library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.library.service.BookService;
+
+@SpringBootApplication
+public class LibraryManagementApplication {
+
+	public static void main(String[] args) {
+		 ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		 
+		 BookService bookService = (BookService) context.getBean("bookService");
+
+		SpringApplication.run(LibraryManagementApplication.class, args);
+	}
+
+}
